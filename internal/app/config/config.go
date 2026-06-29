4f1c344603179f382acb4aec198ca82f97ec1814
RonIT-401 <70mars07@gmail.com>
2026-06-30 00:01:32 +1200

    TASK-1001: реализовал загрузку конфигурации из .env

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -1,1 +1,25 @@
 package config
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+
+	"github.com/RonIT-401/catalog-service/internal/app/config/section"
+)
+
+type Config struct {
+	Repository section.Repository
+	Processor  section.Processor
+	Monitor    section.Monitor
+}
+
+var Root Config
+
+func Load() {
+	_ = godotenv.Load()
+	if err := envconfig.Process("APP", &Root); err != nil {
+		log.Fatal(err)
+	}
+}
--- internal/app/config/section/repository.go
+++ internal/app/config/section/repository.go
@@ -1,1 +1,18 @@
 package section
+
+import "time"
+
+type (
+	Repository struct {
+		Postgres RepositoryPostgres
+	}
+
+	RepositoryPostgres struct {
+		Address      string        `required:"true"`
+		Username     string        `required:"true"`
+		Password     string        `required:"true"`
+		Name         string        `required:"true"`
+		ReadTimeout  time.Duration `split_words:"true"`
+		WriteTimeOut time.Duration `split_words:"true"`
+	}
+)
