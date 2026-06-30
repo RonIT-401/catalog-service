37d22a03c7d5e97369a766bac453b79c7b44fb44
RonIT-401 <70mars07@gmail.com>
2026-06-30 21:52:21 +1200

    Исправил регистр ReadTimeout и добавил default

--- internal/app/config/section/repository.go
+++ internal/app/config/section/repository.go
@@ -12,7 +12,7 @@ type (
 		Username     string        `required:"true"`
 		Password     string        `required:"true"`
 		Name         string        `required:"true"`
-		ReadTimeout  time.Duration `split_words:"true"`
-		WriteTimeOut time.Duration `split_words:"true"`
+		ReadTimeout  time.Duration `default:"30s" split_words:"true"`
+		WriteTimeout time.Duration `default:"30s" split_words:"true"`
 	}
 )
